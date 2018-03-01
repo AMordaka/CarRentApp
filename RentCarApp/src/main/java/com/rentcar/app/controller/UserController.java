25d3f939db197688d65c4d01bea5b4f2b2615302
Arkadiusz <arkadiusz1057@gmail.com>
2018-03-01 21:12:39 +0100

    Wyodrębnienie funkcjonalności do poszczególnych kontrolerów

---
+++ RentCarApp/src/main/java/com/rentcar/app/controller/UserController.java
@@ -0,0 +1,110 @@
+package com.rentcar.app.controller;
+
+import com.rentcar.app.model.User;
+import com.rentcar.app.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Locale;
+
+@Controller
+@RequestMapping("/")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    MessageSource messageSource;
+
+    @RequestMapping(value = { "/newuser" }, method = RequestMethod.GET)
+    public String newUser(ModelMap model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        model.addAttribute("edit", false);
+        model.addAttribute("loggedinuser", userService.getPrincipal());
+        return "registration";
+    }
+
+
+    /**
+     * This method will provide the medium to update an existing user.
+     */
+    @RequestMapping(value = { "/edit-user-{ssoId}" }, method = RequestMethod.GET)
+    public String editUser(@PathVariable String ssoId, ModelMap model) {
+        User user = userService.findBySSO(ssoId);
+        model.addAttribute("user", user);
+        model.addAttribute("edit", true);
+        model.addAttribute("loggedinuser", userService.getPrincipal());
+        return "registration";
+    }
+
+    /**
+     * This method will be called on form submission, handling POST request for
+     * updating user in database. It also validates the user input
+     */
+    @RequestMapping(value = { "/edit-user-{ssoId}" }, method = RequestMethod.POST)
+    public String updateUser(@Valid User user, BindingResult result,
+                             ModelMap model, @PathVariable String ssoId) {
+
+        if (result.hasErrors()) {
+            return "registration";
+        }
+        userService.updateUser(user);
+        model.addAttribute("success", "User " + user.getFirstName() + " "+ user.getLastName() + " updated successfully");
+        model.addAttribute("loggedinuser", userService.getPrincipal());
+        return "registrationsuccess";
+    }
+
+    @RequestMapping(value = { "/delete-user-{ssoId}" }, method = RequestMethod.GET)
+    public String deleteUser(@PathVariable String ssoId) {
+        userService.deleteUserBySSO(ssoId);
+        return "redirect:/list";
+    }
+
+
+    /**
+     * This method will be called on form submission, handling POST request for
+     * saving user in database. It also validates the user input
+     */
+    @RequestMapping(value = { "/newuser" }, method = RequestMethod.POST)
+    public String saveUser(@Valid User user, BindingResult result,	ModelMap model) {
+
+        if (result.hasErrors()) {
+            return "registration";
+        }
+
+        if(!userService.isUserSSOUnique(user.getId(), user.getSsoId())){
+            FieldError ssoError =new FieldError("user","ssoId",messageSource.getMessage("non.unique.ssoId", new String[]{user.getSsoId()}, Locale.getDefault()));
+            result.addError(ssoError);
+            return "registration";
+        }
+
+        userService.saveUser(user);
+
+        model.addAttribute("success", "User " + user.getFirstName() + " "+ user.getLastName() + " registered successfully");
+        model.addAttribute("loggedinuser", userService.getPrincipal());
+        //return "success";
+        return "registrationsuccess";
+    }
+
+    @RequestMapping(value = {"/list" }, method = RequestMethod.GET)
+    public String listUsers(ModelMap model) {
+
+        List<User> users = userService.findAllUsers();
+        model.addAttribute("users", users);
+        model.addAttribute("loggedinuser", userService.getPrincipal());
+        return "userslist";
+    }
+}
